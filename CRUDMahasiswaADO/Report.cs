217c2b02a9df3430caaaf1756c693f9d913762c9
ilham101106 <pesatoffical321@gmail.com>
2026-06-21 19:49:45 +0700

    Add Crystal Report integration

---
+++ CRUDMahasiswaADO/Report.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Report : Form
+    {
+        DAL dbLogic = new DAL();
+        CrystalReport1 listMahasiswa = new CrystalReport1();
+        string prodi { get; set; }
+        DateTime tglmasuk { get; set; }
+
+        public Report(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            try
+            {
+                DataTable dtMahasiswa = dbLogic.getDataRekap(prodi, tglmasuk);
+
+                listMahasiswa.SetDataSource(dtMahasiswa);
+                crystalReportViewer1.ReportSource = listMahasiswa;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
