4848af70dc4161913d4ccaa72b705b1b4e148008
ilham101106 <pesatoffical321@gmail.com>
2026-06-21 19:49:44 +0700

    Add RekapMahasiswa form using DAL

---
+++ CRUDMahasiswaADO/RekapMahasiswa.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CRUDMahasiswaADO
+{
+    partial class RekapMahasiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbProdi = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpTanggalMasuk = new System.Windows.Forms.DateTimePicker();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCetak = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(590, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REKAP DATA MAHASISWA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prodi";
+            // 
+            // cmbProdi
+            // 
+            this.cmbProdi.FormattingEnabled = true;
+            this.cmbProdi.Location = new System.Drawing.Point(262, 112);
+            this.cmbProdi.Name = "cmbProdi";
+            this.cmbProdi.Size = new System.Drawing.Size(238, 28);
+            this.cmbProdi.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(592, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tahun Masuk";
+            // 
+            // dtpTanggalMasuk
+            // 
+            this.dtpTanggalMasuk.CustomFormat = "M/d/yyyy";
+            this.dtpTanggalMasuk.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTanggalMasuk.Location = new System.Drawing.Point(703, 114);
+            this.dtpTanggalMasuk.Name = "dtpTanggalMasuk";
+            this.dtpTanggalMasuk.Size = new System.Drawing.Size(200, 26);
+            this.dtpTanggalMasuk.TabIndex = 4;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnLoad.Location = new System.Drawing.Point(1208, 614);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(137, 44);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCetak.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnCetak.Location = new System.Drawing.Point(1208, 102);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(137, 38);
+            this.btnCetak.TabIndex = 6;
+            this.btnCetak.Text = "Cetak";
+            this.btnCetak.UseVisualStyleBackColor = false;
+            this.btnCetak.Click += new System.EventHandler(this.btnCetak_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(206, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1139, 447);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // RekapMahasiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1540, 686);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dtpTanggalMasuk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbProdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RekapMahasiswa";
+            this.Text = "RekapMahasiswacs";
+            this.Load += new System.EventHandler(this.RekapMahasiswacs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbProdi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpTanggalMasuk;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCetak;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ CRUDMahasiswaADO/RekapMahasiswa.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class RekapMahasiswa : Form
+    {
+        // String koneksi ke database SQL Server kamu
+        static string connectionString = "Data Source=GOHAMSS\\ILHAM;Initial Catalog=DBAkademikADO;User ID=sa;Password=is101106";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        public RekapMahasiswa()
+        {
+            InitializeComponent();
+        }
+
+        // Event saat Form Rekap pertama kali dibuka
+        private void RekapMahasiswacs_Load(object sender, EventArgs e)
+        {
+            // Mengatur format DateTimePicker agar hanya menampilkan Tahun saja
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            // Mengunci ComboBox agar user tidak bisa mengetik manual
+            cmbProdi.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Tombol cetak dimatikan dulu sampai data berhasil di-load
+            btnCetak.Enabled = false;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                // Mengambil daftar prodi untuk mengisi ComboBox Filter
+                SqlCommand cmd = new SqlCommand("select namaprodi from programstudi", conn);
+                cmd.CommandType = CommandType.Text;
+                dtProdi = new DataTable();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(dtProdi);
+
+                cmbProdi.DataSource = dtProdi;
+                cmbProdi.DisplayMember = "namaprodi";
+                cmbProdi.ValueMember = "namaprodi";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data prodi: " + ex.Message);
+            }
+        }
+
+        // Tombol LOAD: Digunakan untuk mengambil data dari DB dan menampilkan ke DataGridView
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                // Memanggil Stored Procedure sp_Report
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Mengirimkan parameter berdasarkan filter yang dipilih user
+                cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = cmbProdi.SelectedValue;
+                cmd.Parameters.Add("@inTglMsuk", SqlDbType.VarChar, 4).Value = dtpTanggalMasuk.Value.Year.ToString();
+
+                da = new SqlDataAdapter(cmd);
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                // Memasukkan data hasil query ke dalam DataGridView (Area Abu-abu)
+                dataGridView1.DataSource = dtMahasiswa;
+
+                // Validasi: Jika data ditemukan, tombol cetak aktif. Jika kosong, tombol cetak mati.
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+                    btnCetak.Enabled = true;
+                }
+                else
+                {
+                    btnCetak.Enabled = false;
+                    MessageBox.Show("Data tidak ditemukan untuk prodi dan tahun tersebut.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data mahasiswa: " + ex.Message);
+            }
+        }
+
+        // Tombol CETAK: Membuka Form Report (Crystal Report) setelah data dipastikan ada
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Membuka Form Report sambil mengoper parameter Prodi dan Tahun
+                Report frm2 = new Report(cmbProdi.SelectedValue.ToString(), dtpTanggalMasuk.Value);
+                frm2.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal membuka laporan: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Diabaikan saja jika tidak digunakan
+        }
+    }
+}
